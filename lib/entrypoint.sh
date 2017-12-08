dd210d5bed38ccf26f69fbf63fc697ce53476341
Rory Stokes <rory.stokes@environment.gov.au>
2017-12-08 17:05:20 +1100

    Tidy entrypoint .sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-if [ -z "$PROXY_URL"]
+if [ ! -z "$PROXY_URL"]
 then
-	# no proxy url defined, do nothing
-	true;
-else
-	# proxy url configured, append proxypass config to httpd.conf
+	# if proxy url configured, append ProxyPass config to httpd.conf
 	cat /var/lib/proxy_cfg.conf >> ${HTTPD_PREFIX}/conf/httpd.conf
 fi
 
