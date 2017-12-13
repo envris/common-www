29c192d3b0d5110c6c3f076d7f64bf5ed50f4608
Simon Kuss <23348455+simonkuss@users.noreply.github.com>
2017-12-14 10:41:01 +1100

    Fix script complaint about ]. Added ProxyPassReverse.

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -z "$PROXY_URL"]
+if [ ! -z "$PROXY_URL" ]
 then
 	# if proxy url configured, append ProxyPass config to httpd.conf
 	cat /var/lib/proxy_cfg.conf >> ${HTTPD_PREFIX}/conf/httpd.conf
